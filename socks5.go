96acb7cddb8c693a18052cc41c47a61271ebde5b
Wankupi <2893353848@qq.com>
2023-07-04 16:59:43 +0800

    fix(socks5): pass simple,errors,concurrent tests

--- socks5.go
+++ socks5.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/binary"
 	"errors"
 	"fmt"
@@ -8,6 +9,7 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -38,6 +40,7 @@ func start_connection(conn net.Conn) {
 	defer conn.Close()
 	server, err := hand_shake(conn)
 	if err != nil {
+		fmt.Print("\033[31m", err, "\033[0m\n")
 		return
 	}
 	defer server.Close()
@@ -47,30 +50,82 @@ func start_connection(conn net.Conn) {
 
 func hand_shake(conn net.Conn) (sever net.Conn, err error) {
 	var buf [260]byte
-	n, err := conn.Read(buf[:])
+	reader := bufio.NewReader(conn)
+	// n, err := reader.Read(buf[:])
+	n, err := io.ReadFull(reader, buf[:2])
+	authSupport := false
 	if err != nil {
 		return nil, errors.New("read from connect failed, code: " + err.Error())
 	}
-	if buf[0] != 0x05 || buf[1] == 0 {
-		return nil, errors.New("version is wrong or nMethods is 0")
+	// if buf[0] != 0x05 {
+	// 	authSupport = true
+	// }
+	n, err = io.ReadFull(reader, buf[:buf[1]])
+	if err != nil {
+		return nil, errors.New("read from connect failed, code: " + err.Error())
+	}
+	for _, val := range buf[:n] {
+		if val == 0x00 {
+			authSupport = true
+			break
+		}
+	}
+	if !authSupport {
+		conn.Write([]byte{0x05, 0xff}) // 不支持任何认证方式
+		return nil, errors.New("no auth method is supported")
 	}
 	conn.Write([]byte{0x05, 0x00}) // 不需要认证
 
-	n, err = conn.Read(buf[:])
+	var isUnsupport bool = false
+	n, err = io.ReadFull(reader, buf[:5])
 	if err != nil {
 		return nil, errors.New("read from connect failed, code: " + err.Error())
+	}
+	if (buf[0] != 0x05) || (buf[1] != 0x01) {
+		conn.Write([]byte{0x05, 0x07, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+		return nil, errors.New("version is wrong or command is not correct")
+	}
+	if buf[3] == 0x01 {
+		m, err := io.ReadFull(reader, buf[5:10])
+		if err != nil {
+			return nil, errors.New("read from connect failed, code: " + err.Error())
+		}
+		n += m
+	} else if buf[3] == 0x03 {
+		m, err := io.ReadFull(reader, buf[5:7+buf[4]])
+		if err != nil {
+			return nil, errors.New("read from connect failed, code: " + err.Error())
+		}
+		n += m
+	} else if buf[3] == 0x04 {
+		m, err := io.ReadFull(reader, buf[5:22])
+		if err != nil {
+			return nil, errors.New("read from connect failed, code: " + err.Error())
+		}
+		n += m
+	} else {
+		isUnsupport = true
 	}
 
 	port := binary.BigEndian.Uint16(buf[n-2 : n])
 	addr := ""
 	switch buf[3] {
 	case 0x01: // ipv4
+		if n != 10 {
+			isUnsupport = true
+		}
 		addr = fmt.Sprintf("%v.%v.%v.%v:%v", buf[4], buf[5], buf[6], buf[7], port)
 		break
 	case 0x03: // domain
+		if n != int(buf[4])+7 {
+			isUnsupport = true
+		}
 		addr = string(buf[5:n-2]) + fmt.Sprintf(":%v", port)
 		break
 	case 0x04: // ipv6
+		if n != 22 {
+			isUnsupport = true
+		}
 		addr = "["
 		for i := 4; i < 20; i += 2 {
 			if i != 4 {
@@ -80,6 +135,12 @@ func hand_shake(conn net.Conn) (sever net.Conn, err error) {
 		}
 		addr += fmt.Sprintf("]:%v", port)
 		break
+	default:
+		isUnsupport = true
+	}
+	if isUnsupport {
+		conn.Write([]byte{0x05, 0x08, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+		return nil, errors.New("unsupported address type")
 	}
 
 	fmt.Printf("[%v] %v\n", time.Now().Format("15:04:05.000"), addr)
@@ -87,7 +148,19 @@ func hand_shake(conn net.Conn) (sever net.Conn, err error) {
 	d := net.Dialer{Timeout: time.Duration(5) * time.Second}
 	server, err := d.Dial("tcp", addr)
 	if err != nil {
-		conn.Write([]byte{0x05, 0x05, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+		var fail_type byte
+		if strings.Contains(err.Error(), "refused") {
+			fail_type = 0x05
+		} else if strings.Contains(err.Error(), "no route") {
+			fail_type = 0x03
+		} else if strings.Contains(err.Error(), "i/o timeout") {
+			fail_type = 0x04
+		} else if strings.Contains(err.Error(), "network is unreachable") {
+			fail_type = 0x03
+		} else if strings.Contains(err.Error(), "failure in name resolution") {
+			fail_type = 0x04
+		}
+		conn.Write([]byte{0x05, fail_type, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
 		return nil, errors.New("dial failed, code: " + err.Error())
 	}
 
